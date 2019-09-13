ddd4d589909178f04bf2f3af725d37db89529929
arpit <arpit769690@gmail.com>
2019-09-13 02:33:05 -0700

    Add project files.

---
+++ National-Bonds/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace National_Bonds.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
